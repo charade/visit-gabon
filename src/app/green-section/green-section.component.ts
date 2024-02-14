d16b9dd4934e86f132731a2c7df59395f49231ae
charade <charles.ekomie@gmail.com>
2024-02-14 22:26:22 +0100

    entering green section transition in progress

--- src/app/green-section/green-section.component.ts
+++ src/app/green-section/green-section.component.ts
@@ -40,19 +40,39 @@ export class GreenSectionComponent implements AfterViewInit {
   }
 
   #enteringGreenSectionContentAnimation(): gsap.core.Timeline {
+    const greenSpeciesTextContent = document.querySelectorAll(
+      '.green-species-description-content > h3'
+    );
+
     return gsap
       .timeline({
         ease: 'sine',
         scrollTrigger: {
           trigger: 'section.green-section',
           start: 'top 99%',
-          end: 'top 99%',
-          scrub: 1.2,
+          end: 'top 90%',
+          scrub: 4,
         },
       })
       .to('.cursor-mask', {
         opacity: 0,
       })
-      .from('.green-section-content', { opacity: 0, xPercent: -100 });
+      .from('.green-section-content', {
+        opacity: 0,
+        xPercent: -150,
+        duration: 5,
+      })
+      .from('.green-species-description-content', {
+        opacity: 0,
+        height: 0,
+        stagger: 1,
+      })
+      .from('.green-species-description-content > h3', {
+        transformOrigin: 'top right',
+        rotateZ: '-45deg',
+        y: 20,
+        opacity: 0,
+        stagger: 0.4,
+      });
   }
 }
