999457e53bb9ad6100f15654f33b90959eb36dbf
charade <charles.ekomie@gmail.com>
2024-03-06 11:51:18 +0100

    refactor design

--- src/app/components/green-section/green-section.component.ts
+++ src/app/components/green-section/green-section.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import gsap from 'gsap';
+import SplitType from 'split-type';
 import { MediaBreakPointsObserver } from '../../utils/breakpoint-observer';
 import { UserAgent } from '../../utils/user-agent';
 
@@ -18,179 +19,144 @@ export class GreenSectionComponent
   @Input() animationTimeline: gsap.core.Timeline;
   UserAgent = UserAgent;
 
-  #greenSectionSecondContentBgCinematic = 'cinematic';
-
-  ngAfterViewInit(): void {
-    this.animationTimeline.add(
-      this.#enteringGreenSectionIntroContentAnimation()
-    );
-
-    this.animationTimeline.add(this.#exitGreenSectionIntroContentAnimation());
-    this.animationTimeline.add(this.#enteringGreenSectionSecondContent());
-  }
-
   readonly greenPics = [
     {
       src: '/assets/pics/green-section/nyonie.jpg',
       alt: 'parc nyonie',
-      animationDuration: '5.5s',
+      animationDuration: '17.5s',
     },
     {
       src: '/assets/pics/green-section/chutes-de-kongou.jpg',
       alt: 'chutes de kongou',
-      animationDuration: '4.8s',
+      animationDuration: '8.8s',
     },
     {
       src: '/assets/pics/green-section/parc-de-loango.jpg',
       alt: 'parc de loango',
-      animationDuration: '4.5s',
+      animationDuration: '14.5s',
     },
     {
       src: '/assets/pics/green-section/parc-des-plateaux-bateke.jpg',
       alt: 'plateaux bateke',
-      animationDuration: '4.1s',
+      animationDuration: '18.1s',
     },
     {
       src: '/assets/pics/green-section/parc-lope.jpg',
       alt: 'parc lopé',
-      animationDuration: '4.9s',
+      animationDuration: '7.9s',
     },
     {
       src: '/assets/pics/green-section/parc-mayumba.jpg',
       alt: 'parc mayumba',
-      animationDuration: '4.7s',
+      animationDuration: '16.7s',
     },
     {
       src: '/assets/pics/green-section/parc-minkebe.jpg',
       alt: 'parc minkébé',
-      animationDuration: '5s',
+      animationDuration: '13s',
     },
     {
       src: '/assets/pics/green-section/parc-pongara.jpg',
       alt: 'parc pongara',
-      animationDuration: '4.5s',
+      animationDuration: '19.5s',
     },
   ];
-  #enteringGreenSectionIntroContentAnimation(): gsap.core.Timeline {
-    return gsap
-      .timeline({
-        ease: 'sine.inOut',
+
+  ngAfterViewInit(): void {
+    this.animationTimeline.add(this.#enteringGreenSectionFirstContent());
+    this.animationTimeline.add(this.#exitGreenSectionFirstContent());
+    this.animationTimeline.add(this.#enteringGreenSectionSecondContent());
+  }
+
+  #enteringGreenSectionFirstContent(): gsap.core.Timeline {
+    const tl = gsap
+      .timeline()
+      .from('.green-section-first-content-catch-phrase', {
+        y: -80,
+        opacity: 0,
         scrollTrigger: {
-          trigger: 'section.green-section-intro',
-          start: 'top 99%',
-          end: 'top 98%',
+          trigger: '.green-section-first-content',
+          start: 'top 43%',
+          end: 'top 43%',
           scrub: 3,
         },
       })
-      .from('.green-section-contents-container', {
-        opacity: 0,
-        xPercent: -150,
-        duration: 4,
-      })
-      .to('.low-video-container', {
-        // reset the hublot video on returning to intro content
-        scale: 1,
-        opacity: 1,
-      })
-      .from('.green-species-description-content', {
-        opacity: 0,
+      .from('.green-section-first-content-species-description-content', {
         height: 0,
-        stagger: 1,
+        stagger: 0.2,
+        delay: 0.2,
+        scrollTrigger: {
+          trigger: '.green-section-first-content',
+          start: 'top 20%',
+          end: 'top 20%',
+          scrub: 3,
+        },
       })
-      .from('.green-species-description-content > h3', {
-        transformOrigin: 'top right',
-        rotateZ: '-30deg',
-        y: 20,
-        opacity: 0,
-        stagger: 0.3,
+      .from('.green-section-first-content-species-description-content > h3', {
+        rotate: '-45deg',
+        y: 25,
+        ease: 'expo.inOut',
+        scrollTrigger: {
+          trigger: '.green-section-first-content',
+          start: 'top 20%',
+          end: 'top 20%',
+          scrub: 3,
+        },
       });
+
+    const splittedText = new SplitType(
+      '.green-section-first-content-species-description-content'
+    );
+
+    tl.from(splittedText.words, {
+      height: 0,
+      stagger: 0.9,
+      duration: 1,
+      scrollTrigger: {
+        trigger: '.green-section-first-content',
+        start: 'top -3%',
+        end: 'top -5%',
+        scrub: 3,
+      },
+    });
+
+    return tl;
   }
 
-  #exitGreenSectionIntroContentAnimation(): gsap.core.Timeline {
-    return gsap
-      .timeline({
-        scrollTrigger: {
-          trigger: 'section.green-section-intro',
-          start: 'top center',
-          end: 'top 45%',
-          scrub: 4,
-        },
-      })
-      .to('.low-video-container', {
-        scale: 0.3,
-        opacity: 0,
-        duration: 2,
-      })
-      .to('.green-section-intro-content > *', {
-        x: 100,
-        opacity: 0,
-        stagger: 0.3,
-      })
-      .from('.green-section-second-content', {
-        opacity: 0,
-      })
-
-      .from(
-        '.green-section-second-content-bg',
-        {
-          xPercent: -30,
-          opacity: 0,
-          filter: 'grayscale(0)',
-          ease: 'expo.out',
-          duration: 3,
-        },
-        this.#greenSectionSecondContentBgCinematic
-      );
+  #exitGreenSectionFirstContent(): gsap.core.Tween {
+    return gsap.to('.green-section-first-content', {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: '.green-section-first-content',
+        start: 'bottom 10%',
+        end: 'bottom 10%',
+        scrub: 3,
+      },
+    });
   }
 
   #enteringGreenSectionSecondContent(): gsap.core.Timeline {
     return gsap
-      .timeline({
+      .timeline()
+      .from('.green-section-second-content-bg', {
+        scale: 0.5,
         scrollTrigger: {
-          trigger: 'section.green-section-intro',
-          start: 'top 45%',
-          end: 'top 40%',
+          trigger: '.green-section-second-content',
+          start: 'top 60%',
+          end: 'top 60%',
           scrub: 3,
         },
       })
-
-      .from(
-        '.separator.first',
-        { width: 0 },
-        this.#greenSectionSecondContentBgCinematic + '+=52'
-      )
-      .from(
-        '.green-section-second-content-catch-phrase',
-        {
-          y: 20,
-          opacity: 0,
+      .from('.green-section-second-content-bg', {
+        scale: 1,
+        immediateRender: false,
+        scrollTrigger: {
+          trigger: '.green-section-second-content',
+          start: 'bottom 45%',
+          end: 'bottom 45%',
+          toggleActions: 'play reverse none reverse',
+          scrub: 3,
         },
-        this.#greenSectionSecondContentBgCinematic + '+=58'
-      )
-      .from(
-        '.green-section-second-content-description > p',
-        {
-          y: -40,
-          opacity: 0,
-        },
-        this.#greenSectionSecondContentBgCinematic + '+=58'
-      )
-      .fromTo(
-        '.green-section-second-content-explore',
-        {
-          opacity: 0,
-          clipPath: 'polygon(0% 0%, 100% 100%, 100% 100%, 0% 100%)',
-        },
-        {
-          opacity: 1,
-          height: '30rem',
-          clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
-        }
-      )
-      .from('.green-section-second-content-explore-content', {
-        opacity: 0,
-        xPercent: -100,
-        ease: 'sine.out',
       });
   }
 }
--- src/app/components/yellow-section/yellow-section.component.ts
+++ src/app/components/yellow-section/yellow-section.component.ts
@@ -15,54 +15,5 @@ export class YellowSectionComponent
 {
   @Input() animationTimeline: gsap.core.Timeline;
 
-  ngAfterViewInit(): void {
-    this.animationTimeline.add(this.#enteringYellowSection());
-    this.animationTimeline.add(this.#exitYellowSection());
-  }
-
-  #enteringYellowSection(): gsap.core.Timeline {
-    return gsap
-      .timeline({
-        scrollTrigger: {
-          trigger: 'section.yellow-section',
-          start: 'top 75%',
-          end: 'top 75%',
-          scrub: 2,
-        },
-      })
-      .from('.yellow-section-container', {
-        xPercent: -100,
-        ease: 'expo.out',
-      });
-  }
-
-  #exitYellowSection(): gsap.core.Timeline {
-    return gsap
-      .timeline({
-        scrollTrigger: {
-          trigger: 'section.yellow-section',
-          start: 'bottom bottom',
-          end: 'bottom top',
-          scrub: 3,
-        },
-      })
-      .to(
-        'h2.yellow-section-catch-phrase-container',
-        {
-          y: -100,
-          opacity: 0,
-          ease: 'power2',
-        },
-        'exit yellow section'
-      )
-      .to(
-        '.yellow-section-bg-clipper path',
-        {
-          y: '+0.1%',
-          opacity: 0,
-          ease: 'power2',
-        },
-        'exit yellow section'
-      );
-  }
+  ngAfterViewInit(): void {}
 }
